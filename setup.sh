dff41d858594aa834fe3b66363988f78a68bc5af
Tonio Lora <tonio.lora@microsoft.com>
2021-10-18 08:51:11 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# This script runs the Terraform commands.
+#
+#   @Azure:~$ git clone https://github.com/shaneochotny/Azure-Synapse-Analytics-PoC
+#   @Azure:~$ cd Azure-Synapse-Analytics-PoC
+#   @Azure:~$ nano terraform.tfvars
+#   @Azure:~$ setup.sh
+#   @Azure:~$ bash configure.sh
+#
+
+# Make sure this configuration script hasn't been executed already
+if [ -f "setup.complete" ]; then
+    echo "ERROR: It appears this setup has already been completed.";
+    exit 1;
+fi
+
+# Make sure we have all the required artifacts
+terraform init
+terraform plan
+terraform apply
+
+echo "Setup complete!"
+touch setup.complete
