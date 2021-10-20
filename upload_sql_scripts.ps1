3231206cf4a6125897d1d6c3575cb7d3b7186c76
Tonio Lora <tonio.lora@microsoft.com>
2021-10-20 11:25:24 -0500

    Update upload_sql_scripts.ps1
    Added Synapse Serverless Folder

--- upload_sql_scripts.ps1
+++ upload_sql_scripts.ps1
@@ -1,7 +1,5 @@
 # Import Sql scripts to workspace
-# Change hardcoded version of <workspace-name> and replace during setup 
 $workspaceName = 'REPLACE_SYNAPSE_ANALYTICS_WORKSPACE_NAME'
-$sqlScriptFileFolder = './artifacts/synapse_data_security/'
 
 # Get token for data plane
 $token = Get-AzAccessToken -ResourceUrl https://dev.azuresynapse.net
@@ -11,8 +9,25 @@ $authHeader = @{
 }
  
 # Read sql script file and save content to body variable
+$sqlScriptFileFolder = './artifacts/synapse_data_security/'
 Get-ChildItem $sqlScriptFileFolder | ForEach-Object -Process {
-    $body = '{"name":"<sql-script-name>","properties":{"folder":{"name":"Synapse Data Security"},"content":{"query":"<sql-script>","metadata":{"language":"sql"},"currentConnection":{"databaseName":"master","poolName":"Built-in"},"resultLimit":5000},"type":"SqlQuery"}}'
+    $body = '{"name":"<sql-script-name>","properties":{"folder":{"name":"<script folder name>"},"content":{"query":"<sql-script>","metadata":{"language":"sql"},"currentConnection":{"databaseName":"master","poolName":"Built-in"},"resultLimit":5000},"type":"SqlQuery"}}'
+    $folderName = 'Synapse Data Security'
+    $body = $body -replace '<script folder name>', $folderName
+    $sqlScriptName = $_.BaseName
+    $body = $body -replace '<sql-script-name>', $sqlScriptName
+    $script = Get-Content -Raw $_
+    $body = $body -replace '<sql-script>', $script
+
+    # Send request to create SQL script in a workspace
+    Invoke-RestMethod -Method PUT -Uri https://${workspaceName}.dev.azuresynapse.net/sqlscripts/${sqlScriptName}?api-version=2020-12-01 -Body $body -Headers $authHeader
+}
+
+$sqlScriptFileFolder = './artifacts/synapse_serverless/'
+Get-ChildItem $sqlScriptFileFolder | ForEach-Object -Process {
+    $body = '{"name":"<sql-script-name>","properties":{"folder":{"name":"<script folder name>"},"content":{"query":"<sql-script>","metadata":{"language":"sql"},"currentConnection":{"databaseName":"master","poolName":"Built-in"},"resultLimit":5000},"type":"SqlQuery"}}'
+    $folderName = 'Synapse Serverless'
+    $body = $body -replace '<script folder name>', $folderName
     $sqlScriptName = $_.BaseName
     $body = $body -replace '<sql-script-name>', $sqlScriptName
     $script = Get-Content -Raw $_
