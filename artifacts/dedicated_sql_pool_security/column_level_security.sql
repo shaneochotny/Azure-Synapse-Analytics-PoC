6bf56a16a1a92209a591594b6d7defe6f241132e
Tonio-Lora <anlo@microsoft.com>
2022-07-28 16:23:33 -0500

    All reamaining security SQL Scripts added

---
+++ artifacts/dedicated_sql_pool_security/column_level_security.sql
@@ -0,0 +1,48 @@
+/* Use Case: 
+    DataAnalystSFO should not have access to birthdate and phone columns
+    on the Customer table
+*/
+
+-- Create 2 users for use case demonstration!
+CREATE USER DataAnalystSFO WITHOUT LOGIN;
+CREATE USER DataAnalystGlobal WITHOUT LOGIN;
+--SELECT * FROM sys.sysusers WHERE name in ('DataAnalystGlobal', 'DataAnalystSFO');
+
+-- If users already existed, revoke access to Sample.DimCustomer 
+REVOKE SELECT ON Sample.DimCustomer FROM DataAnalystSFO, DataAnalystGlobal;
+
+-- Allow DataAnalystGlobal access to all columns on Sample.DimCustomer Table.
+GRANT SELECT ON Sample.DimCustomer TO DataAnalystGlobal, DataAnalystSFO;
+
+-- Allow DataAnalystSFO access to a subset of columns on Sample.DimCustomer Table.
+GRANT SELECT ON Sample.DimCustomer(
+[CustomerKey],[GeographyKey],[CustomerAlternateKey],[Title],[FirstName],[MiddleName],[LastName],[NameStyle]
+--,[BirthDate],[Phone]
+,[MaritalStatus],[Suffix],[Gender],[EmailAddress],[YearlyIncome],[TotalChildren],[NumberChildrenAtHome],[EnglishEducation]
+,[SpanishEducation],[FrenchEducation],[EnglishOccupation],[SpanishOccupation],[FrenchOccupation],[HouseOwnerFlag]
+,[NumberCarsOwned],[AddressLine1],[AddressLine2],[DateFirstPurchase],[CommuteDistance]
+) TO DataAnalystSFO;
+
+-- Check whether DataAnalystSFO has select access to the all columns
+EXECUTE AS USER ='DataAnalystSFO';
+select TOP 100 * from Sample.DimCustomer;
+
+-- Check whether DataAnalystSFO has select access to the other columns
+EXECUTE AS USER ='DataAnalystSFO';
+select
+[CustomerKey],[GeographyKey],[CustomerAlternateKey],[Title],[FirstName],[MiddleName],[LastName],[NameStyle]
+--,[BirthDate],[Phone]
+,[MaritalStatus],[Suffix],[Gender],[EmailAddress],[YearlyIncome],[TotalChildren],[NumberChildrenAtHome],[EnglishEducation]
+,[SpanishEducation],[FrenchEducation],[EnglishOccupation],[SpanishOccupation],[FrenchOccupation],[HouseOwnerFlag]
+,[NumberCarsOwned],[AddressLine1],[AddressLine2],[DateFirstPurchase],[CommuteDistance]
+ from Sample.DimCustomer;
+
+-- Check whether DataAnalystGlobal has select access to the all columns
+EXECUTE AS USER ='DataAnalystGlobal';
+select TOP 100 * from Sample.DimCustomer;
+
+/*
+CLEAN UP:
+DROP USER DataAnalystSFO;
+DROP USER DataAnalystGlobal;
+*/
---
+++ artifacts/dedicated_sql_pool_security/dynamic_data_masking.sql
@@ -0,0 +1,51 @@
+/* Use Case: 
+    Mask & Unmask the EmailAddress and BirthDate columns of the 
+    Customer table to DataAnalystSFO
+*/
+
+-- Create 2 users for use case demonstration!
+CREATE USER DataAnalystSFO WITHOUT LOGIN;
+CREATE USER DataAnalystGlobal WITHOUT LOGIN;
+--SELECT * FROM sys.sysusers WHERE name in ('DataAnalystGlobal', 'DataAnalystSFO');
+
+-- Grant SELECT permission to 'DataAnalystSFO' on the Customer table
+GRANT SELECT ON Sample.DimCustomer TO DataAnalystSFO;  
+
+-- Run a query as 'DataAnalystSFO' to verify the access 
+EXECUTE AS USER = 'DataAnalystSFO';  
+SELECT TOP 100 * FROM Sample.DimCustomer;
+
+-- Optionally, Check for existing Dynamic Data Masking applied on columns
+SELECT c.name, tbl.name as table_name, c.is_masked, c.masking_function  
+FROM sys.masked_columns AS c  
+JOIN sys.tables AS tbl
+    ON c.[object_id] = tbl.[object_id]  
+WHERE is_masked = 1
+    AND tbl.name = 'DimCustomer';
+
+-- Mask 'EmailAddress' and 'BirthDate' columns of Customer table
+ALTER TABLE Sales.DimCustomer
+ALTER COLUMN EmailAddress ADD MASKED WITH (FUNCTION = 'email()');
+GO
+ALTER TABLE Sales.DimCustomer
+ALTER COLUMN BirthDate ADD MASKED WITH (FUNCTION = 'default()');
+GO
+
+
+
+-- Validate the data for DataAnalystSFO
+EXECUTE AS USER = 'DataAnalystSFO';  
+SELECT TOP 100 * FROM Sample.DimCustomer;
+
+-- Perform the Unmask operation
+GRANT UNMASK TO DataAnalystSFO;
+EXECUTE AS USER = 'DataAnalystSFO';  
+SELECT TOP 100 * FROM Sample.DimCustomer;
+
+/*
+CLEAN UP:
+DROP USER DataAnalystSFO;
+DROP USER DataAnalystGlobal;
+ALTER TABLE Sample.DimCustomer ALTER COLUMN EmailAddress DROP MASKED;
+ALTER TABLE Sample.DimCustomer ALTER COLUMN BirthDate DROP MASKED;
+*/
